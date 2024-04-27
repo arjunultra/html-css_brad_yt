781c892d6d7e89368be024098b8fff6e5334e9ea
Arjun Arasu <arjunbalan1994@gmail.com>
2024-04-27 12:14:33 +0530

    accordian functionality achieved

--- main.js
+++ main.js
@@ -2,6 +2,15 @@
 document.addEventListener ('DOMContentLoaded',function(){
     const faqContainer = document.querySelector('.faq-content')
     faqContainer.addEventListener('click',function(e) {
-        console.log(e)
+       const groupHeader = e.target.closest('.faq-group-header');
+       if(!groupHeader) return;
+       const group = groupHeader.parentElement;
+       const groupBody = group.querySelector('.faq-group-body')
+       const icon = groupHeader.querySelector('i')
+    //    toggle icon
+    icon.classList.toggle('fa-plus')
+    icon.classList.toggle('fa-minus')
+    // Toggle Visibility of Body
+    groupBody.classList.toggle('open')
     })
 })
