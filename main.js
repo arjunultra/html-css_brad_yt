7b88ce605b3796b09f991e97951607d3d9f3ddc9
Arjun Arasu <arjunbalan1994@gmail.com>
2024-04-27 11:24:00 +0530

    accordian JS started

---
+++ main.js
@@ -0,0 +1,7 @@
+// FAQ Accordian open close
+document.addEventListener ('DOMContentLoaded',function(){
+    const faqContainer = document.querySelector('.faq-content')
+    faqContainer.addEventListener('click',function(e) {
+        console.log(e)
+    })
+})
