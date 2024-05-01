a87a94958d6312858543b7a1c008a68920288947
Arjun Balan <arjunbalan1994@gmail.com>
2024-05-01 12:02:43 +0530

    project finished

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
 // FAQ Accordian open close
-document.addEventListener ('DOMContentLoaded',function(){
-    const faqContainer = document.querySelector('.faq-content')
-    faqContainer.addEventListener('click',function(e) {
-       const groupHeader = e.target.closest('.faq-group-header');
-       if(!groupHeader) return;
-       const group = groupHeader.parentElement;
-       const groupBody = group.querySelector('.faq-group-body')
-       const icon = groupHeader.querySelector('i')
+document.addEventListener("DOMContentLoaded", function () {
+  const faqContainer = document.querySelector(".faq-content");
+  faqContainer.addEventListener("click", function (e) {
+    const groupHeader = e.target.closest(".faq-group-header");
+    if (!groupHeader) return;
+    const group = groupHeader.parentElement;
+    const groupBody = group.querySelector(".faq-group-body");
+    const icon = groupHeader.querySelector("i");
     //    toggle icon
-    icon.classList.toggle('fa-plus')
-    icon.classList.toggle('fa-minus')
+    icon.classList.toggle("fa-plus");
+    icon.classList.toggle("fa-minus");
     // Toggle Visibility of Body
-    groupBody.classList.toggle('open')
+    groupBody.classList.toggle("open");
 
     // Close other open FAQ Bodies
-    const otherGroups = faqContainer.querySelectorAll('.faq-group')
-    otherGroups.forEach((otherGroup)=>{
-        if (otherGroup!==group) {
-            const otherGroupBody = otherGroup.querySelector('.faq-group-body')
-            const otherIcon = otherGroup.querySelector('.faq-group-header i')
-            otherGroupBody.classList.remove('open')
-            otherIcon.classList.remove('fa-minus')
-            otherIcon.classList.add('fa-plus')
-        }
-    })
-    })
-})
+    const otherGroups = faqContainer.querySelectorAll(".faq-group");
+    otherGroups.forEach((otherGroup) => {
+      if (otherGroup !== group) {
+        const otherGroupBody = otherGroup.querySelector(".faq-group-body");
+        const otherIcon = otherGroup.querySelector(".faq-group-header i");
+        otherGroupBody.classList.remove("open");
+        otherIcon.classList.remove("fa-minus");
+        otherIcon.classList.add("fa-plus");
+      }
+    });
+  });
+});
+// Hamburger mobile menu
+
+const hamburgerBtn = document.getElementById("hamburger-btn");
+const mobileMenu = document.querySelector(".mobile-menu");
+hamburgerBtn.addEventListener("click", function () {
+  mobileMenu.classList.toggle("active");
+});
